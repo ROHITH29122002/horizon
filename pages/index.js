33d33f166c95c6e6f4bf01b951375090b5cd50c5
Rohith Challa <rohithsince2002@gmail.com>
2023-02-25 18:38:55 +0530

    desc sec update

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <>
+    <div>
       <div className={styles.hero}>
         <Navbar />
         <div className={styles.hero_center}>
@@ -13,6 +13,28 @@ export default function Home() {
         </div>
         <div></div>
       </div>
-    </>
+      <div className={styles.about}>
+        <div className={styles.about_left}>
+          <h1>ABOUT</h1>
+          <h1>HORIZON</h1>
+        </div>
+        <div className={styles.about_right}>
+          <div className={styles.desc_cont}>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultrices venenatis feugiat. Ut posuere nunc sit amet porta volutpat. Donec accumsan libero in elementum condimentum. Donec placerat nibh ipsum.</h3>
+          </div>
+          <div className={styles.more_desc_cont}>
+            <div>
+              <h3>2 DAYS</h3>
+            </div>
+            <div>
+              <h3>10+ EVENTS</h3>
+            </div>
+            <div>
+              <h3>COUNTDOWN</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
